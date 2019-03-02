cd224b19843dbae68752f6d6762af7399a0c7f34
Marcin Mroz <s13835@pjwstk.edu.pl>
2019-03-02 13:41:14 +0100

    Added initial songs router

--- routes/songs.ts
+++ routes/songs.ts
@@ -1,9 +1,36 @@
 import express from "express";
+import mongodb, { MongoError } from "mongodb";
 import { Song } from "../models/Song";
+
+const MongoClient = mongodb.MongoClient;
 const router = express.Router();
+const uri = process.env.MONGO_DB;
 
 router.get("/", (req, res) => {
-  res.json({ xd: "1" });
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  // tslint:disable-next-line: arrow-parens
+  client.connect(err => {
+    if (err) {
+      res.send(err.message);
+    }
+    const dbQuery = { moods: "" };
+    const queryStr = req.query;
+    if (queryStr) {
+      dbQuery.moods = queryStr.mood;
+    }
+    const collection = client.db("musicjinn").collection("songs");
+
+    collection
+      .find(dbQuery.moods ? dbQuery : {})
+      .toArray()
+      .then((value: Song[]) => {
+        res.send(value);
+        client.close();
+      })
+      .catch((reason: MongoError) => {
+        res.send(reason.message);
+      });
+  });
 });
 
 export default router;
