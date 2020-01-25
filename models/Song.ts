2ebb77efa3b38023fd31717221772bf867e01897
Marcin Mroz <s13835@pjwstk.edu.pl>
2020-01-25 20:24:22 +0100

    Changed to mongoose

--- models/Song.ts
+++ models/Song.ts
@@ -1,8 +1,17 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-export type Song = mongoose.Document & {
+export interface ISong extends Document {
   name: string;
   author: string;
   genre: string;
   moods: string[];
-};
+}
+
+const SongSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  moods: [String]
+});
+
+export default mongoose.model<ISong>("Song", SongSchema);
--- routes/songs.ts
+++ routes/songs.ts
@@ -1,48 +1,38 @@
 import express from "express";
-import mongodb, { MongoError } from "mongodb";
+import mongoose from "mongoose";
 import MoodsMap from "../helpers/MoodsMap";
 import moodsDict from "../helpers/moodsOppositesDict";
-import { Song } from "../models/Song";
+import Song, { ISong } from "../models/Song";
 
-const MongoClient = mongodb.MongoClient;
 const router = express.Router();
 const uri = process.env.MONGO_DB;
 
 const moodsMap = new MoodsMap(moodsDict);
 
 router.get("/", (req, res) => {
-  const client = new MongoClient(uri, { useNewUrlParser: true });
-  // tslint:disable-next-line: arrow-parens
-  client.connect(err => {
-    if (err) {
-      res.send(err.message);
-    }
-    const dbQuery = { moods: "" };
-    const queryStr = req.query;
-    const { mood } = queryStr;
-    if (queryStr) {
-      if (queryStr.wantToStay === "true") {
-        dbQuery.moods = mood;
-      } else {
-        const oppositeMood = moodsMap.getOppositeMood(mood);
-        if (oppositeMood) {
-          dbQuery.moods = oppositeMood;
-        }
+  const dbQuery = { moods: "" };
+  const queryStr = req.query;
+  const { mood } = queryStr;
+  if (queryStr) {
+    if (queryStr.wantToStay === "true") {
+      dbQuery.moods = mood;
+    } else {
+      const oppositeMood = moodsMap.getOppositeMood(mood);
+      if (oppositeMood) {
+        dbQuery.moods = oppositeMood;
       }
     }
-    const collection = client.db("musicjinn").collection("songs");
-    // tslint:disable-next-line: no-console
-    console.log(dbQuery);
-    collection
-      .find(dbQuery)
-      .toArray()
-      .then((value: Song[]) => {
-        res.send(value);
-        client.close();
-      })
-      .catch((reason: MongoError) => {
-        res.send(reason.message);
-      });
+  }
+
+  mongoose.connect(uri, { useNewUrlParser: true });
+  const db = mongoose.connection;
+  db.once("open", async () => {
+    try {
+      const queryResult: ISong[] = await Song.find(dbQuery).exec();
+      res.send(queryResult);
+    } catch (e) {
+      res.status(500).send(e);
+    }
   });
 });
 
